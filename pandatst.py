07c1685842a09ffa951b9c31e778b348e380315f
Min Zhu <min.mindyzhu@gmail.com>
2018-02-08 20:48:45 -0500

    checkin

--- pandatst.py
+++ pandatst.py
@@ -1,3 +1,6 @@
+
+# This is a test file to try on on data wrangling techniques with pandas
+
 # import matplotlib.pyplot as plt
 # import numpy as np
 # import pandas as pd
@@ -10,19 +13,29 @@
 # df['YEAR.BUILT'].diff().hist()
 
 
-# The usual preamble
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# import csv file
 df=pd.read_csv('tc1sales.csv')
 
+# cat var count of each category
 zones_counts = df['zone1'].value_counts()
+print zones_counts
 
+# cut cols from original df
 dfsh=df[['neighbor','pricepsf','grosqft.x']]
 
+# another way to show category counts
 nbhd_counts=dfsh.groupby('neighbor').size()
-nbhd_counts.index = ['asto','coro','elmh','JH','LIC']
-nbhd_counts.plot(kind='bar')
+print nbhd_counts
 
+# change index: label nbhd names
+nbhd_counts.index = ['asto','coro','elmh','JH','LIC']
+print nbhd_counts
+# nbhd_counts.plot(kind='bar')
+
+# aggregate values
 dfsh.groupby('neighbor').aggregate(sum)
-dfsh.groupby('neighbor').median().add_prefix('median_')
+print dfsh.groupby('neighbor').median().add_prefix('median_')
+
