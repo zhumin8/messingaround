8b0b2699854973d0fee0b642af3974a0a737259a
ZhuM <min.mindyzhu@gmail.com>
2018-02-08 23:07:41 -0500

    add plots

--- pandatst.py
+++ pandatst.py
@@ -14,10 +14,24 @@
 
 
 import pandas as pd
+
+
+# do this before importing pylab or pyplot
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 
 # import csv file
 df=pd.read_csv('tc1sales.csv')
+df['SALE.DATE']=pd.to_datetime(df['SALE.DATE'])
+
+# do first plot and output, then clear
+plot1=df.plot.scatter(x='GROSS.SQUARE.FEET', y='pricepsf');
+fig1=plot1.get_figure()
+fig1.savefig('test1.png')
+plt.clf()
+
 
 # cat var count of each category
 zones_counts = df['zone1'].value_counts()
@@ -33,9 +47,31 @@ print nbhd_counts
 # change index: label nbhd names
 nbhd_counts.index = ['asto','coro','elmh','JH','LIC']
 print nbhd_counts
-# nbhd_counts.plot(kind='bar')
+
+# do second plot and output
+plot= nbhd_counts.plot(kind='barh')
+fig = plot.get_figure()
+fig.savefig('test.png')
+plt.clf()
 
 # aggregate values
 dfsh.groupby('neighbor').aggregate(sum)
 print dfsh.groupby('neighbor').median().add_prefix('median_')
 
+# try seaborn plots
+import seaborn as sns
+sns.set(color_codes=True)
+plot2 = sns.distplot(df['pricepsf']);
+fig2 = plot2.get_figure()
+fig2.savefig('sns_tst.png')
+plt.clf()
+
+plot = sns.jointplot(x="GROSS.SQUARE.FEET", y="pricepsf", data=df, kind="kde");
+#fig = plot.get_figure()
+plot.savefig('sns_tst2.png')
+plt.clf()
+
+
+plot = sns.pairplot(df[['pricepsf','story','grosqft.x','yrbuilt']]);
+plot.savefig('sns_tst3.png')
+plt.clf()
